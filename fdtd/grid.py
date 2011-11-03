3804e72a814ff2029075c8b56e39b7de3a32a910
Patrik Gebhardt <grosser.knuff@googlemail.com>
2011-11-03 18:12:15 +0100

    grid reworked and solver added

--- fdtd/grid.py
+++ fdtd/grid.py
@@ -4,9 +4,11 @@ import scipy.interpolate
 class grid:
     """Defines the calculation domain"""
     def __init__(self, deltaX, deltaY, xSize, ySize):
-        self.hField = numpy.zeros( (xSize/deltaX, ySize/deltaY) )
-        self.eField = numpy.zeros( (xSize/deltaX, ySize/deltaY) )
+        # create even and odd Grid
+        self.evenGrid = numpy.array( (xSize/deltaX, ySize/deltaY) )
+        self.oddGrid = numpy.array( (xSize/deltaX - 1, ySize/deltaY - 1) )
         
+        # save all given information
         self.deltaX = deltaX
         self.deltaY = deltaY
         self.xSize = xSize
---
+++ fdtd/solver.py
@@ -0,0 +1,15 @@
+import numpy
+from grid import grid
+from material import material
+
+class solver:
+    """Solves FDTD equations on given grid, with given materials and ports"""
+    def __init__(self, grid, material, ports=None):
+        # save arguments
+        self.grid = grid
+        self.material = material
+        self.ports = ports
+
+    def iterate(self, deltaT, timesteps)
+        """Iterates the FDTD algorithm in respect of the pre-defined ports"""
+        pass
