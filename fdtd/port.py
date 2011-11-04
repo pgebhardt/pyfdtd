407ae15de756fd54238923368fed6f44d98423c3
Patrik Gebhardt <grosser.knuff@googlemail.com>
2011-11-04 07:20:38 +0100

    Revert c2080f2ba90f97b6b6e4b8d8a26dd00c8665c4af^..HEAD

--- fdtd/port.py
+++ fdtd/port.py
@@ -3,12 +3,13 @@ from grid import grid
 
 class port:
     """I/O Port to connect Grids to outer world"""
-    def __init__(self, position):
+    def __init__(self, position, function=None):
         # save arguments
         self.position = position
+        self.function = function
         self.values = []
 
-    def update(self, grid, value=0.0, mode='append'):
+    def update(self, grid, t=0.0, mode='append'):
         """Updates data port and connected grid"""
         
         # get result value
@@ -16,10 +17,13 @@ class port:
         x, y = int(x/grid.deltaX), int(y/grid.deltaY)
         self.values.append([grid.oddGrid[x, y]])
 
-        # apply value change
-        if mode == 'append':
-            grid.oddGrid[x, y] += value
-        elif mode == 'set':
-            grid.oddGrid[x, y] = value
-        else:
-            raise AttributeError('mode')
+        # apply function
+        if self.function:
+            f = self.function(t)
+        
+            if mode == 'append':
+                grid.oddGrid[x, y] += value
+            elif mode == 'set':
+                grid.oddGrid[x, y] = value
+            else:
+                raise AttributeError('mode')
