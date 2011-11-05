9c53538d71ac82ab9915eb6c71de4b9604b3ed42
Patrik Gebhardt <grosser.knuff@googlemail.com>
2011-11-05 01:33:26 +0100

    Revert dc7e6ac551e0a056c328fee1e544c68158a60a80^..HEAD

--- fdtd/material.py
+++ fdtd/material.py
@@ -5,9 +5,12 @@ class material:
     """Descripes the material"""
     def __init__(self, xSize, ySize, deltaX, deltaY):
         # init grids
-        self.oddGrid = {}
-        self.evenGridX = {}
-        self.evenGridY = {}
+        xShape = xSize/deltaX
+        yShape = ySize/deltaY
+
+        self.oddGrid = {'epsilon': numpy.ones((xShape, yShape)), 'mu': numpy.ones((xShape, yShape)), 'sigma': numpy.zeros((xShape, yShape))}
+        self.evenGridX = {'epsilon': numpy.ones((xShape+1, yShape+1)), 'mu': numpy.ones((xShape+1, yShape+1)), 'sigma': numpy.zeros((xShape+1, yShape+1))}
+        self.evenGridY = {'epsilon': numpy.ones((xShape+1, yShape+1)), 'mu': numpy.ones((xShape+1, yShape+1)), 'sigma': numpy.zeros((xShape+1, yShape+1))}
 
         # save atributes
         self.xSize = xSize
@@ -15,22 +18,5 @@ class material:
         self.deltaX = deltaX
         self.deltaY = deltaY
 
-    def set_material(self, material):
-        # fill odd Grid
-        for key in material.iterkeys():
-            xShape, yShape = material[key].shape
-            
-            # interpolate grid
-            x = numpy.arange(0.0, self.xSize, self.xSize/xShape)
-            y = numpy.arange(0.0, self.ySize, self.ySize/yShape)
-            f = scipy.interpolate.interp2d(x, y, material[key].transpose(), kind='cubic')
-
-            # fill grids
-            self.oddGrid[key] = numpy.zeros( (self.xSize/self.deltaX, self.ySize/self.deltaY) )
-            self.evenGridX[key] = numpy.zeros( (self.xSize/self.deltaX+1, self.ySize/self.deltaY+1) )
-            self.evenGridY[key] = numpy.zeros( (self.xSize/self.deltaX+1, self.ySize/self.deltaY+1) )
-            for i in range(0, int(self.xSize/self.deltaX), 1):
-                for j in range(0, int(self.ySize/self.deltaY), 1):
-                    self.oddGrid[key][i, j] = f(i*self.deltaX, j*self.deltaY)
-                    self.evenGridX[key][i, j] = f(i*self.deltaX, (j-0.5)*self.deltaY)
-                    self.evenGridY[key][i, j] = f((i-0.5)*self.deltaX, j*self.deltaY)
+    def set_material(self, key, material):
+        pass
