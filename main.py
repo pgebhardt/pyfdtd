3d7a1d8aba73bc1d39f0ea73ed3223b8d490d860
Patrik Gebhardt <grosser.knuff@googlemail.com>
2011-11-18 08:25:29 +0100

    Just simulated a lense for testing

--- main.py
+++ main.py
@@ -12,22 +12,22 @@ def f(t):
 
 # material function
 def mat(x, y):
-    if x - 0.02*math.sin(2.0*math.pi*5.0*y) - 0.3 > 0.0:
-        return 59.1e6
+    if (x-0.2)**2 + (y-0.15)**2 < 0.15**2 and (x-0.2)**2 + (y-0.35)**2 < 0.15**2:
+        return 2.0
 
-    return 0.0
+    return 1.0
         
 # create solver
-solver = pyfdtd.solver(pyfdtd.field(0.4, 0.4, deltaX=0.001))
+solver = pyfdtd.solver(pyfdtd.field(0.4, 0.8, deltaX=0.001))
 
 # add material
-solver.material['sigma'] = mat
+solver.material['epsilon'] = mat
 
 # add source
-solver.ports.append(pyfdtd.port(0.1, 0.2, function=f))
+solver.ports.append(pyfdtd.port(0.2, 0.1, function=f))
 
 # iterate
-history = solver.solve(5e-9, saveHistory=True)
+history = solver.solve(7e-9, saveHistory=True)
 
 # show plot
 fig = plt.figure(1)
