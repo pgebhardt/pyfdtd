44ac5794028f6674ac92ff7ed4d2154d7f5165d1
Patrik Gebhardt <grosser.knuff@googlemail.com>
2011-11-21 13:15:18 +0100

    Package is now v0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyfdtd',
-    version='0.1.0',
+    version='0.2.0',
     description='2D FDTD using numpy',
     long_description='pyfdtd is a 2D electromagnetic fieldsolver in time-domain using numpy.',
     author=', '.join((
