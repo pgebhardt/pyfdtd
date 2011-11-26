e0b7e3b638f973826f64e226e977bdcd02ed3d17
Patrik Gebhardt <grosser.knuff@googlemail.com>
2011-11-26 10:39:45 +0100

    closing issue #22

--- pyfdtd/source.py
+++ pyfdtd/source.py
@@ -1,5 +1,5 @@
 def source(function):
-    """Creates a valid sourcefunction of the given function"""
+    """Decorator to create a valid sourcefunction"""
     def res(flux, deltaT, t):
         return -0.5*deltaT*function(t-0.5*deltaT)
 
