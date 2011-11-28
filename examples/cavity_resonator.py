f599c39217f92e3728f3204f5fc3f93372697f36
Patrik Gebhardt <grosser.knuff@googlemail.com>
2011-11-28 09:43:38 +0100

    source corrected and listener added

--- examples/cavity_resonator.py
+++ examples/cavity_resonator.py
@@ -3,23 +3,23 @@ import numpy
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.colors as colors
-import pyfdtd
+from pyfdtd import *
 
 # source function
-@pyfdtd.source
+@source
 def f(t):
     x = t - 1000e-12
     return 1.0e3*math.exp(-x**2/(2.0*200.0e-12**2))*math.cos(2.0*math.pi*20e9*x)
 
 # create solver
-solver = pyfdtd.solver(pyfdtd.field(0.2, 0.4, deltaX=0.001))
+solver = solver(field(0.2, 0.4, deltaX=0.001))
 
 # add material
-solver.material['electric'][:,:] = pyfdtd.material.epsilon(sigma=59.1e6)
-solver.material['electric'][0.07:0.13,0.05:-0.05] = pyfdtd.material.epsilon()
+solver.material['electric'][:,:] = material.epsilon(sigma=59.1e6)
+solver.material['electric'][0.07:0.13,0.05:-0.05] = material.epsilon()
 
 # add source
-solver.source[pyfdtd.masks.ellipse(0.1, 0.1, 0.001)] = f
+solver.source[masks.ellipse(0.1, 0.1, 0.001)] = f
 
 # iterate
 history = solver.solve(5e-9, saveHistory=True)
--- pyfdtd/source.py
+++ pyfdtd/source.py
@@ -1,20 +1,7 @@
-import numpy
+def source(function):
+    """Decorator to create a valid sourcefunction"""
+    def res(flux, deltaT, t):
+        return -0.5*deltaT*function(t)
 
-class source:
-    @staticmethod
-    def currentdensity(function):
-        """Decorator to create a valid sourcefunction"""
-        def res(flux, deltaT, t):
-            return -0.5*deltaT*function(t)
-
-        # return sourcefunction
-        return res
-
-    @staticmethod
-    def fluxdensity(function):
-        """ """
-        def res(flux, deltaT, t):
-            return -0.5*(function(t)-function(t-deltaT))
-
-        # return sourcefunction
-        return res
+    # return sourcefunction
+    return res
