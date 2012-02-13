a00418ed98dd25f117e7755b684729c40cd5ec0e
Schansge <grosser.knuff@googlemail.com>
2012-02-13 11:24:24 +0100

    licence added to each file

--- pyfdtd/masks.py
+++ pyfdtd/masks.py
@@ -1,3 +1,20 @@
+# pyfdtd is a simple 2d fdtd using numpy
+# Copyright (C) 2012  Patrik Gebhardt
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 def ellipse(posx, posy, rx, ry=None):
     """Creates a mask function with circle shape"""
     # set default for ry
